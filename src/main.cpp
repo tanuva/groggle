db9c5f979533544c5b91aeac6f47e0fbeadde19c
Marcel Kummer <tanuva@posteo.de>
2019-01-26 20:39:11 +0100

    Add another hack to switch colors on audio peaks

--- src/main.cpp
+++ src/main.cpp
@@ -69,27 +69,58 @@ void sendValue(const int channel, const float v)
     }
 }
 
+static void hslToRgb(const float hsl[3], float *rgb)
+{
+    // Source: https://en.wikipedia.org/wiki/HSL_and_HSV#Alternative_HSL_conversion
+    const float a = hsl[1] * std::min(hsl[2], 1.0f - hsl[2]);
+    static const auto k = [hsl](const int n) {
+        // (n + H / 30) mod 12
+        // The modulus shall preserve the fractional component.
+        const float tmp = n + hsl[0] / 30.0f;
+        const float frac = tmp - floor(tmp);
+        return (static_cast<int>(floor(tmp)) % 12) + frac;
+    };
+    static const auto f = [hsl, a](const int n) {
+        return hsl[2] - a * std::max(
+                                -1.0f,
+                                std::min(
+                                    1.0f,
+                                    std::min(k(n) - 3.0f,
+                                             9.0f - k(n))));
+    };
+
+    rgb[0] = f(0);
+    rgb[1] = f(8);
+    rgb[2] = f(4);
+}
+
 void sendSpectrum(const Spectrum spectrum)
 {
     /* Tripar:
      * 1-3: RGB
      * 6: Dimmer
      */
+    static const int NUM_COLORS = 12;
+    static float hsl[3] {0, 1.0f, 0.5f};
 
     static float lastVal;
     float val = spectrum.get(Band::LOW);
     if ( val > lastVal ) {
         lastVal = val;
-        // TODO Trigger color change here. Beat detection!
+        hsl[0] += 360 / NUM_COLORS;
     } else {
         lastVal *= 0.9f;
         val = lastVal;
     }
 
+    float rgb[3];
+    hslToRgb(hsl, rgb);
+
     // TODO Prepare one DMX buffer per frame, don't send every time
     sendValue(ADJ + 5, val);
-    sendValue(ADJ + 0, 1.0);
-    sendValue(ADJ + 1, 0.25);
+    sendValue(ADJ + 0, rgb[0]);
+    sendValue(ADJ + 1, rgb[1]);
+    sendValue(ADJ + 2, rgb[2]);
 }
 
 // Try to lower the sample rate if the pi is too slow
