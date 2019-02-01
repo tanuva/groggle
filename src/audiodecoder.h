923ad833fb7b35dd5bd534471e95b9d51c22568e
Marcel Kummer <tanuva@posteo.de>
2019-02-01 21:49:14 +0100

    Removed unused audiodecoder.{h,cpp}

--- src/audiodecoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUDIODECODER
-#define AUDIODECODER
-
-#include <inttypes.h>
-#include <stddef.h> // size_t
-
-int decode_audio_file(const char *path, int16_t **data, size_t *size, float *duration);
-
-#endif
