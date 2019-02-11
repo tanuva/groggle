cf49d2999b5b4843ec4e0a7f8c8e723332b9277a
Marcel Kummer <tanuva@posteo.de>
2019-02-11 20:34:45 +0100

    Bail out if SDL initialization failed

--- src/main.cpp
+++ src/main.cpp
@@ -392,6 +392,7 @@ int main(const int argc, const char **argv)
 
     if (SDL_Init(SDL_INIT_AUDIO) != 0) {
         SDL_Log("SDL_Init failed: %s", SDL_GetError());
+        return -1;
     }
     atexit(&cleanup);
 
