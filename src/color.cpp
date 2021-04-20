8fac0f742fbbc64da86decf0e39ffe22f26fa5fc
Marcel Kummer <tanuva@posteo.de>
2021-04-20 21:32:35 +0200

    Fix HSL->RGB color conversion
    Turns out copying one Color instance in static helper functions isn't
    going to work. Let's go the old fashioned way with static C helper
    functions. Probably faster, too.

--- src/color.cpp
+++ src/color.cpp
@@ -25,28 +25,29 @@ Color::Color(const Color &other)
     toRgb();
 }
 
+static float k(const int n, const float h)
+{
+    // (n + H / 30) mod 12
+    // The modulus shall preserve the fractional component.
+    const float tmp = n + h / 30.0f;
+    const float frac = tmp - std::floor(tmp);
+    return (static_cast<int>(floor(tmp)) % 12) + frac;
+}
+
+static float f(const int n, const float a, const float h, const float l)
+{
+    // f(n) = L - a * max(-1, min(k - 3, 9 - k, 1))
+    return l - a * std::max(-1.0f,
+                            std::min(1.0f,
+                                     std::min(k(n, h) - 3.0f,
+                                              9.0f - k(n, h))));
+}
+
 void Color::toRgb()
 {
     // Source: https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB_alternative
     const float a = m_s * std::min(m_l, 1.0f - m_l);
-    static const auto k = [this](const int n) {
-        // (n + H / 30) mod 12
-        // The modulus shall preserve the fractional component.
-        const float tmp = n + m_h / 30.0f;
-        const float frac = tmp - std::floor(tmp);
-        return (static_cast<int>(floor(tmp)) % 12) + frac;
-    };
-    static const auto f = [this, a](const int n) {
-        // f(n) = L - a * max(-1, min(k - 3, 9 - k, 1))
-        return l() - a * std::max(
-                                -1.0f,
-                                std::min(
-                                    1.0f,
-                                    std::min(k(n) - 3.0f,
-                                             9.0f - k(n))));
-    };
-
-    m_r = f(0);
-    m_g = f(8);
-    m_b = f(4);
+    m_r = f(0, a, m_h, m_l);
+    m_g = f(8, a, m_h, m_l);
+    m_b = f(4, a, m_h, m_l);
 }
